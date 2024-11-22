1181f9d5ba1f41a795cbdc1c5c185e551f31be53
neha <nehasaraswaat106@gmail.com>
2024-11-22 16:35:03 +0530

    dssfjd

--- main.tf
+++ main.tf
@@ -4,3 +4,16 @@ module "ec2" {
   myinstance_type = var.instance_type_loc
   
 }
+
+module "s3" {
+  source = "./modules/s3"
+}
+
+module "vpc" {
+  source   = "./modules/vpc"
+  vpc_name = "my-vpc"
+}
+
+
+
+
